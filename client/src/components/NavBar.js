fecec58f818ac70b784bee1420655b2539f132bf
sarahwambui96 <sarahwambui843@gmail.com>
2023-08-09 23:03:16 +0300

    changed pay

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const NavBar = ({isLoggedIn}) => {
+const NavBar = () => {
 
     
     return (
-        <nav>
-            {!isLoggedIn ? ( <> <Link to='/login'> Login</Link>
-            <Link to='/signup'> SignUp</Link></> ) :  (            
-<>            <Link to='/'>Event</Link>
+        <nav>        
+            <Link to='/'>Event</Link>
             <Link to='/newevent'> Create Event</Link>
-            <Link to='/about'> Our Story</Link></>)   }
-
-
-
+            <Link to='/about'> Our Story</Link>
+            <Link to='/login'> Login</Link>
+            <Link to='/signup'> SignUp</Link> 
         </nav>
     )
 }
--- client/src/components/TicketCheckout.js
+++ client/src/components/TicketCheckout.js
@@ -4,7 +4,7 @@ import { useParams, Link } from 'react-router-dom';
 function TicketCheckout() {
     let [count, setCount] = useState(1);
     const [checkout, setCheckout]= useState([])
-    // console.log(count)
+    console.log(count)
     const {id}=useParams()
     // console.log(id)
    
@@ -29,7 +29,10 @@ function TicketCheckout() {
     }
     let product = count * checkout.ticket_price
 
+    let remaining_tickets = checkout.available_tickets - count
+    console.log(remaining_tickets)
 
+    
 
     return (
         <div>
@@ -39,7 +42,8 @@ function TicketCheckout() {
                 <p>Ticket Number:{checkout.id}</p>
                 <p>Event Date:{checkout.date_time}</p>
                 <p>Price: {checkout.ticket_price}</p>
-                {/* <p>Tickets: {count}</p> */}
+                <p>Available Tickets: {checkout.available_tickets}</p>
+                <p>Tickets: {count}</p>
                 <button onClick={handleIncrement}> <span> + </span></button>
                 <span> {count} </span>
                 <button onClick={handleDecrement}><span> - </span></button>
